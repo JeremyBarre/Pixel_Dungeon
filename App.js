8932298c14de1cad80283781b6e56df8532e720e
Jérémy Barré <73218559+JeremyBarre@users.noreply.github.com>
2022-03-07 20:00:03 +0100

    Test2

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Non non non, vous n'avez pas dit le mot magique !</Text>
+      <Text>Saperlipopuut !!</Text>
       <StatusBar style="auto" />
     </View>
   );
